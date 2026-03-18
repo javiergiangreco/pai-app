e69380c04a74f56938568a583564968ce82f3f55
Javier Esteban Giangreco <javiergiangreco@gmail.com>
2026-03-18 17:34:53 -0300

    Add files via upload

--- app_web.py
+++ app_web.py
@@ -133,7 +133,7 @@ def validar_final(borrador, modo):
 # --- 5. INTERFAZ PRINCIPAL ---
 col1, col2 = st.columns([1, 15]) # Los números manejan la proporción del espacio
 with col1:
-    st.image("icono.png", width=100)
+    st.image("icono.png", width=80)
 with col2:
     st.title("PAI")
 st.caption("Pausa Anti Impulsividad")
